bf37cce58d5d94e644b443ea43d3517a3e82b063
Sanchay Thalnerkar <thalnerkarsanchay17@gmail.com>
2022-11-12 20:16:20 +0530

    final

--- leadgenerator/settings.py
+++ leadgenerator/settings.py
@@ -100,9 +100,9 @@ WSGI_APPLICATION = "leadgenerator.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "NAME": env("DATABASE_NAME"),
+        "NAME": env("DATABASES_NAME"),
         "USER": env("DATABASES_USER"),
-        "PASSWORD": env("DATABASES_USER"),
+        "PASSWORD": env("DATABASES_PASSWORD"),
         "HOST": env("DATABASES_HOST"),
         # "HOST": "localhost",
         "PORT": env("DATABASES_PORT"),
@@ -199,3 +199,18 @@ STRONGHOLD_PUBLIC_NAMED_URLS = (
     "password_reset_complete",
 )
 STRONGHOLD_PUBLIC_URLS = ("/admin/",)
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "tailwind"
+CRISPY_TEMPLATE_PACK = "tailwind"
+
+if not DEBUG:
+    SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    SECURE_BROWSER_XSS_FILTER = True
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    SECURE_HSTS_SECONDS = 31536000  # 1 year
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
+    X_FRAME_OPTIONS = "DENY"
